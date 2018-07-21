753ebf0ac526422ebea07ee8d8690e95d58c41d6
Andrejus <andrejus@think-engineer.com>
2018-07-21 22:42:45 +0100

    Add device data as children

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import { request } from 'https';
 
 admin.initializeApp()
 
@@ -7,13 +8,32 @@ export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!")
 })
 
+/**
+ * Attempts to insert body.
+ * 200 - Success.
+ * 400 - DB Error.
+ */
 export const pushData = functions.https.onRequest((request, response) => {
-    /* Get data */
-    let data = request.body
-    /* Set data */
-    admin.database().ref('/dump').set(data)
-    /* Verify data */
-    admin.database().ref('/dump').once('value').then((snapshot) => {
-        response.status(200).send(JSON.stringify(snapshot))
+    let data = {
+        time: Date.now(),
+        data: request.body
+    }
+    admin.database().ref('/data').push(data).once('child_added').then(value => {
+        response.status(200).send(value)
+    }, reason => {
+        response.status(400).send(reason)
+    })
+})
+
+/**
+ * Attempts to get and return data.
+ * 200 - Success.
+ * 400 - DB Error.
+ */
+export const getData = functions.https.onRequest((request, response) => {
+    admin.database().ref('/data').once('value').then(value => {
+        response.status(200).send(value)
+    }, reason => {
+        response.status(400).send(reason)
     })
 })
