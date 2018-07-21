3d16f68bf00e154a9d19659fcab6b5bf1e7d95f7
Andrejus <andrejus@think-engineer.com>
2018-07-21 22:28:15 +0100

    Add Firebase datastore echo function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,19 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+    response.send("Hello from Firebase!")
+})
+
+export const pushData = functions.https.onRequest((request, response) => {
+    /* Get data */
+    let data = request.body
+    /* Set data */
+    admin.database().ref('/dump').set(data)
+    /* Verify data */
+    admin.database().ref('/dump').once('value').then((snapshot) => {
+        response.status(200).send(JSON.stringify(snapshot))
+    })
+})
