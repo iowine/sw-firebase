5ea5473575141c6b57a341df0e12e3bcba68432d
Andrejus <andrejus@think-engineer.com>
2018-11-18 15:57:36 +0000

    Remove debug function logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,12 +15,6 @@ export const helloWorld = functions.https.onRequest((_, response) => {
  */
 export const pushData = functions.https.onRequest((request, response) => {
 
-    /* Debug log request body */
-    console.log(request.body)
-
-    /* Put in raw data */
-    admin.database().ref('/raw').push(request.body)
-
     /* Parse request */
     const device = request.body.device
     const data = {
@@ -46,7 +40,6 @@ export const pushData = functions.https.onRequest((request, response) => {
  * 400 - DB Error.
  */
 export const getData = functions.https.onRequest((request, response) => {
-    console.log(request)
     admin.database().ref('/data').once('value').then(value => {
         response.status(200).send(value)
     }, reason => {
