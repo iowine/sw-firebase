7b849cd33a8134ddf4c026179e5ebc490813323b
Andrejus <andrejus@think-engineer.com>
2018-11-24 17:55:51 +0000

    Integrate Firebase with ng2-charts

--- public/src/app/device-view/device-view.component.ts
+++ public/src/app/device-view/device-view.component.ts
@@ -10,12 +10,18 @@ import { Observable } from 'rxjs';
 })
 export class DeviceViewComponent implements OnInit {
 
-  route: ActivatedRoute
-  db: AngularFireDatabase
+  /** Back-end variables */
+  private route: ActivatedRoute
+  private db: AngularFireDatabase
 
+  /** Front-end variables */
   device
-  deviceRef: AngularFireList<any>
   deviceData: Observable<any[]> = new Observable()
+
+  temperatureData = [{ data: [], label: 'Temperature' }]
+  humidityData = [{ data: [], label: 'Humidity' }]
+  chartType = 'line'
+  chartLabels = []
 
   constructor(route: ActivatedRoute, db: AngularFireDatabase) {
     this.route = route
@@ -26,9 +32,36 @@ export class DeviceViewComponent implements OnInit {
     /* Get device name */
     this.device = this.route.snapshot.params.device
     /* Get device ref */
-    this.deviceRef = this.db.list(`devices/${this.device}/data`)
+    let deviceRef = this.db.list(`devices/${this.device}/data`)
     /* Get observable of database */
-    this.deviceData = this.deviceRef.valueChanges()
+    this.deviceData = deviceRef.valueChanges()
+    /* Subscribe to deviceData */
+    this.deviceData.subscribe(data => {
+      this.update(data)
+    }, error => {
+      console.error(error)
+    })
+  }
+
+  update(data) {
+    /* Wipe existing data */
+    this.temperatureData[0].data.length = 0
+    this.humidityData[0].data.length    = 0
+    /* Limit incoming data */
+    data = data.slice(Math.max(data.length - 100, 1))
+    /* For every datapoint */
+    data.forEach(dataPoint => {
+      /* Add to temperature */
+      this.temperatureData[0].data.push({
+        X: dataPoint.time,
+        y: dataPoint.data.temperature
+      })
+      /* Add to humidity */
+      this.humidityData[0].data.push({
+        X: dataPoint.time,
+        y: dataPoint.data.humidity
+      })
+    })
   }
 
 }
