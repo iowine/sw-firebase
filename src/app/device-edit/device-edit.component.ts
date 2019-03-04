09ac7e8b8c42de7f9620fff97c57a8820df5e0f2
Andrejus <andrejus@think-engineer.com>
2019-03-04 00:51:08 +0000

    Implement device edit and fix scale and time buttons not working

--- src/app/device-edit/device-edit.component.ts
+++ src/app/device-edit/device-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-device-edit',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceEditComponent implements OnInit {
 
-  constructor() { }
+  /* Device name */
+  device: String
 
-  ngOnInit() {
+  db: AngularFireDatabase
+
+  devicesRef: AngularFireList<any>
+  deviceData: Observable<any[]> = new Observable()
+
+  private route: ActivatedRoute
+  private routeSubscriber: Subscription // For device
+
+  constructor(route: ActivatedRoute, db: AngularFireDatabase) {
+    this.route = route
+    this.db = db
+  }
+  ngOnInit() { 
+    this.routeSubscriber = this.route.params.subscribe(params => {
+      /* Get new device's name */
+      this.device = params.device
+      /* Get device ref */
+      this.devicesRef = this.db.list(`devices/${this.device}`)
+      /* Get device names */
+      this.deviceData = this.devicesRef.snapshotChanges()
+      console.log(this.deviceData)
+    })
+  }
+  ngOnDestroy(): void {
+    this.routeSubscriber.unsubscribe()
+  }
+
+  onName(value) {
+    this
+      .db
+      .object(`/devices/${this.device}`)
+      .update({ name: value })
+  }
+
+  onDesc(value) {
+    this
+      .db
+      .object(`/devices/${this.device}`)
+      .update({ desc: value })
   }
 
 }
