b62119f5ad6bb53d8fabef7862e89a9e1858fa57
Andrejus <git@andrejus.uk>
2019-03-08 12:06:44 +0000

    Pull commit hash environment variable in git.version.js

--- git.version.js
+++ git.version.js
@@ -1,7 +1,13 @@
 const { gitDescribeSync } = require('git-describe');
 const { writeFileSync } = require('fs');
 
-const gitInfo = gitDescribeSync();
-const versionInfoJson = JSON.stringify(gitInfo, null, 2);
+try {
+  const gitInfo = gitDescribeSync();
+  const versionInfoJson = JSON.stringify(gitInfo, null, 2);
 
-writeFileSync('git.version.json', versionInfoJson);
+  writeFileSync('git.version.json', versionInfoJson);
+} catch (e) {
+  writeFileSync('git.version.json', {
+    raw: process.env.commit
+  })
+}
