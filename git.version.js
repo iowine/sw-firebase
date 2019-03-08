261a609347a3f2b9ac0aef7fc6548c9f7f49eac4
Andrejus <git@andrejus.uk>
2019-03-08 12:18:07 +0000

    JSON'ify env variable in git.version.js

--- git.version.js
+++ git.version.js
@@ -1,13 +1,22 @@
-const { gitDescribeSync } = require('git-describe');
-const { writeFileSync } = require('fs');
+const { gitDescribeSync } = require('git-describe')
+const { writeFileSync } = require('fs')
 
 try {
-  const gitInfo = gitDescribeSync();
-  const versionInfoJson = JSON.stringify(gitInfo, null, 2);
+  
+  const gitInfo = gitDescribeSync()
+  
+  const versionInfoJson = JSON.stringify(gitInfo, null, 2)
 
-  writeFileSync('git.version.json', versionInfoJson);
+  writeFileSync('git.version.json', versionInfoJson)
+  
 } catch (e) {
-  writeFileSync('git.version.json', {
+  
+  const gitInfo = {
     raw: process.env.commit
-  })
+  }
+  
+  const versionInfoJson = JSON.stringify(gitInfo, null, 2)
+  
+  writeFileSync('git.version.json', versionInfoJson)
+  
 }
