34cb38bdbcbe7a1cc571a7ced700cc8700c786bb
Andrejus <andrejus@think-engineer.com>
2019-04-21 17:54:32 +0100

    Log git.version.js

--- git.version.js
+++ git.version.js
@@ -1,6 +1,8 @@
 const { gitDescribeSync } = require('git-describe')
 const { writeFileSync } = require('fs')
 
+console.log('Getting git commit hash...')
+
 try {
   
   const gitInfo = gitDescribeSync()
@@ -9,6 +11,8 @@ try {
 
   writeFileSync('git.version.json', versionInfoJson)
   
+  console.log(`Using local commit ${gitInfo.raw}`)
+  
 } catch (e) {
   
   const gitInfo = {
@@ -16,7 +20,9 @@ try {
   }
   
   const versionInfoJson = JSON.stringify(gitInfo, null, 2)
-  
+
   writeFileSync('git.version.json', versionInfoJson)
+  
+  console.log(`Using Cloud Build commit ${gitInfo.raw}`)
   
 }
