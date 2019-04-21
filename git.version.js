53effd3d546378681c55130e5265417a55f78fe6
Andrejus <andrejus@think-engineer.com>
2019-04-21 20:14:52 +0100

    Pass commit environment variable to git.version.json

--- git.version.js
+++ git.version.js
@@ -1,6 +1,6 @@
 const { writeFileSync } = require('fs')
 
-const versionInfoJson = JSON.stringify({ raw: process.env.commit }, null, 2)
+const versionInfoJson = JSON.stringify({ raw: process.env.COMMIT }, null, 2)
 writeFileSync(`${__dirname}/git.version.json`, versionInfoJson)
 
 console.log(`Using commit ${versionInfoJson.raw}`)  
