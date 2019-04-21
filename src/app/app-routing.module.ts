f353e9ac1009c0fcfc415303e10a31cee22ec845
Andrejus <andrejus@think-engineer.com>
2019-04-21 23:41:49 +0100

    Route mobile views differently

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { DeviceViewComponent } from './device-view/device-view.component';
 import { DeviceListComponent } from './device-list/device-list.component';
 import { HomeComponent } from './home/home.component';
 import { DeviceEditComponent } from './device-edit/device-edit.component';
 
-const routes: Routes = [
+const desktop_routes: Routes = [
   {
     path: '',
     component: HomeComponent
@@ -26,8 +26,33 @@ const routes: Routes = [
   }
 ];
 
+const mobile_routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'device',
+    component: DeviceListComponent,
+  },
+  {
+    path: 'device/edit/:device',
+    component: DeviceEditComponent
+  },
+  {
+    path: 'device/view/:device',
+    component: DeviceViewComponent
+  },
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(desktop_routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+   constructor(router: Router) {
+    if (window.innerWidth < 768) router.resetConfig(mobile_routes)
+  }
+
+ }
