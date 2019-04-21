bf94374ea933873d227b18ce167b65d6125111ab
Andrejus <andrejus@think-engineer.com>
2019-04-21 19:43:28 +0100

    Fix missing version info reference

--- git.version.js
+++ git.version.js
@@ -3,4 +3,4 @@ const { writeFileSync } = require('fs')
 const versionInfoJson = JSON.stringify({ raw: process.env.SHORT_SHA }, null, 2)
 writeFileSync(`${__dirname}/git.version.json`, versionInfoJson)
 
-console.log(`Using commit ${gitInfo.raw}`)  
+console.log(`Using commit ${versionInfoJson.raw}`)  
