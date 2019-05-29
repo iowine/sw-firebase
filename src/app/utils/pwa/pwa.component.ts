336978982d09af0f9036eb7eabe112de9ddcb771
Andrejus <andrejus@think-engineer.com>
2019-05-29 13:52:15 +0100

    Nest utilities under utils folder

--- src/app/utils/pwa/pwa.component.ts
+++ src/app/utils/pwa/pwa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PwaService } from '../pwa.service';
+import { PwaService } from '../../pwa.service';
 
 @Component({
   selector: 'app-pwa',
